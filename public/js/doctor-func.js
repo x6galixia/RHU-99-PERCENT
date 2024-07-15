a73ef570fafcfc7747b01ba72d7f84817536c7b5
x6galixia <147425442+x6galixia@users.noreply.github.com>
2024-07-15 10:47:10 +0800

    database documentation

---
+++ public/js/doctor-func.js
@@ -0,0 +1,15 @@
+document.getElementById("openVitals").addEventListener("click", function() {
+    document.getElementById("vitalForm").style.display = "block";
+});
+
+document.getElementById("openPrescribe").addEventListener("click", function() {
+    document.getElementById("prescribeForm").style.display = "block";
+});
+
+document.getElementById("openLabReq").addEventListener("click", function() {
+    document.getElementById("labRequestForm").style.display = "block";
+});
+
+document.getElementById("openLabRes").addEventListener("click", function() {
+    document.getElementById("labResultForm").style.display = "block";
+});
