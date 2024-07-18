457bc5834c25450b605ca26c3bc3edc5b9217375
Floressia <adrianesilverogarcia@gmail.com>
2024-07-18 12:31:06 +0800

    Created the branch ian

--- public/js/doctor-func.js
+++ public/js/doctor-func.js
@@ -139,3 +139,25 @@ document.addEventListener("DOMContentLoaded", function() {
         });
   });
   
+
+  function toggleDropdown() {
+    document.getElementById("dropdownContent").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+  function logout() {
+    alert("Logging out...");
+    // Perform logout actions here, such as redirecting to a logout endpoint
+  }
+  
