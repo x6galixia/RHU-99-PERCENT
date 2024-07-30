5a2612b4d6e80f08f6595d063802727cdf686939
chrizza03 <chrizzamae25@gmail.com>
2024-07-30 10:38:43 +0800

    fixed the bug on alert message

--- public/js/pharmacy-func.js
+++ public/js/pharmacy-func.js
@@ -6,15 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
   if (!sessionStorage.getItem("alertShown")) {
     // Show the alert
     alertMessage.style.display = "block";
+    closeAlertBtn.style.display = "block";
 
     // Add event listener to close button
     closeAlertBtn.addEventListener("click", function () {
       alertMessage.style.display = "none";
+      closeAlertBtn.style.display = "none";
       // Set the alert as shown in sessionStorage
       sessionStorage.setItem("alertShown", "true");
     });
   } else {
     // Hide the alert if it has been shown in this session
     alertMessage.style.display = "none";
+    closeAlertBtn.style.display = "none";
   }
 });
