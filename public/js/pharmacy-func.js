5d5bc1b78cad6444f14f4e0bff705b9a4c75f6b0
x6galixia <x6galixia@gmail.com>
2024-07-31 18:44:44 +0800

    fixed a bug in alert message

--- public/js/pharmacy-func.js
+++ public/js/pharmacy-func.js
@@ -1,24 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const alertMessage = document.getElementById("alertMessage");
   const closeAlertBtn = document.getElementById("closeAlertBtn");
-
-  // Check if the alert has been shown in this session
-  if (!sessionStorage.getItem("alertShown")) {
-    // Show the alert
-    alertMessage.style.display = "block";
-    closeAlertBtn.style.display = "block";
-
-    // Add event listener to close button
-    closeAlertBtn.addEventListener("click", function () {
-      alertMessage.style.display = "none";
-      closeAlertBtn.style.display = "none";
-      // Set the alert as shown in sessionStorage
-      sessionStorage.setItem("alertShown", "true");
-    });
-  } else {
-    // Hide the alert if it has been shown in this session
+  
+  // Show the alert
+  alertMessage.style.display = "block";
+  closeAlertBtn.style.display = "block";
+  
+  // Add event listener to close button
+  closeAlertBtn.addEventListener("click", function () {
     alertMessage.style.display = "none";
     closeAlertBtn.style.display = "none";
-  }
+  });
 });
 
